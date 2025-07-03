3655f38dc7eba8573176809107502943290d9498
gaganvdeveloper <gaganvdeveloper@gmail.com>
2025-07-03 10:09:45 +0530

    Project Completed

---
+++ src/main/java/com/mydating/dating/dao/UserDao.java
@@ -0,0 +1,35 @@
+package com.mydating.dating.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.mydating.dating.entity.User;
+import com.mydating.dating.repository.UserRepository;
+import com.mydating.dating.util.UserGender;
+
+@Repository
+public class UserDao {
+
+	@Autowired
+	UserRepository repository;
+
+	public User saveUser(User user) {
+		return repository.save(user);
+	}
+
+	public List<User> findAllMaleUsers() {
+		return repository.findByGender(UserGender.MALE);
+	}
+
+	public List<User> findAllFemaleUsers() {
+		return repository.findByGender(UserGender.FEMALE);
+	}
+
+	public Optional<User> findUserById(int id) {
+		return repository.findById(id);
+	}
+
+}
